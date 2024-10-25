f5a1b7d84e265365d427e340bb50cfdb4758b118
Cyrus Najmi-Vannini <csn2@rice.edu>
2024-10-25 01:06:27 -0500

    Fixed -x flag bug.

--- realloc.c
+++ realloc.c
@@ -3,73 +3,79 @@
 #include <string.h>
 #include "ir.h"
 
-
 // Function Declarations
-void rename_registers(struct Instruction* ir);
-void print_allocated_code(struct Instruction* ir);
-int handleThree(struct Instruction* currOp, int VRName, int index);
-int handleStore(struct Instruction* currOp, int VRName, int index);
-int handleLoad(struct Instruction* currOp, int VRName, int index);
-int handleLoadI(struct Instruction* currOp, int VRName);
-void print_renamed_code(struct Instruction* ir);
+void rename_registers(struct Instruction *ir);
+void print_allocated_code(struct Instruction *ir);
+int handleThree(struct Instruction *currOp, int VRName, int index);
+int handleStore(struct Instruction *currOp, int VRName, int index);
+int handleLoad(struct Instruction *currOp, int VRName, int index);
+int handleLoadI(struct Instruction *currOp, int VRName);
+void print_renamed_code(struct Instruction *ir);
 
 // Global Maps
-int* SRtoVR = NULL;
-int* LU = NULL;
+int *SRtoVR = NULL;
+int *LU = NULL;
 
 // Global Variables
 int maxlive = 0;
 int livecount = 0;
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // Handle -h flag
-    if (argc > 1 && strcmp(argv[1], "-h") == 0) {
+    if (argc > 1 && strcmp(argv[1], "-h") == 0)
+    {
         printf("Command Syntax:\n");
         printf("    ./412alloc k filename\n");
         printf("    k is the number of registers to use (3-64)\n");
         return 0;
     }
-    
-    // Check arguments
-    if (argc != 3) {
-        fprintf(stderr, "Usage: 412alloc k filename\n");
-        return 1;
+    else if (argc > 1 && strcmp(argv[1], "-x") != 0)
+    {
+        // Check arguments
+        if (argc != 3)
+        {
+            fprintf(stderr, "Usage: 412alloc k filename\n");
+            return 1;
+        }
+
+        // Parse k value
+        int k = atoi(argv[1]);
+        if (k < 3 || k > 64)
+        {
+            fprintf(stderr, "ERROR: k must be between 3 and 64\n");
+            return 1;
+        }
     }
-    
-    // Parse k value
-    int k = atoi(argv[1]);
-    if (k < 3 || k > 64) {
-        fprintf(stderr, "ERROR: k must be between 3 and 64\n");
-        return 1;
-    }
-    
+
     // Build IR using your Lab 1 code
-    struct Instruction* ir = buildIR(argv[2]);
-    if (ir == NULL) {
+    struct Instruction *ir = buildIR(argv[2]);
+    if (ir == NULL)
+    {
         return 1; // Error already printed by buildIR
     }
-    
+
     // Perform register renaming
     rename_registers(ir);
 
     // Print out the new IR!
-    //printIR2(ir);
+    // printIR2(ir);
 
     // Print the renamed code
     print_renamed_code(ir);
-    
+
     // Print the transformed code
-    //print_allocated_code(ir);
-    
+    // print_allocated_code(ir);
+
     // Clean up
     // TODO: Add cleanup code for IR
-    
+
     return 0;
 }
 
 // Function to perform register allocation
-void rename_registers(struct Instruction* ir) {
+void rename_registers(struct Instruction *ir)
+{
 
     // Your counter for VRnames
     int VRName = 0;
@@ -77,43 +83,54 @@ void rename_registers(struct Instruction* ir) {
     int mapsize = opcount * 3;
     SRtoVR = malloc(mapsize * sizeof(int));
     LU = malloc(mapsize * sizeof(int));
-    for (int i = 0; i < mapsize; i++) {
+    for (int i = 0; i < mapsize; i++)
+    {
         SRtoVR[i] = -1;
         LU[i] = -1;
     }
 
     // Main loop over the operations, bottom to top
-    struct Instruction* currOp = ir->prev->prev;
+    struct Instruction *currOp = ir->prev->prev;
     int index = (currOp->line) - 1;
-    while (currOp->line != 0) {
+    while (currOp->line != 0)
+    {
         int currCode = currOp->opcode;
         // printf("line: %d\n", currOp->line);
         // printf("currCode: %d\n\n", currCode);
 
         // There are basically five versions that I'm going to do, based on the operand.
-        if(currCode == ADD || currCode == SUB || currCode == MULT || currCode == LSHIFT || currCode == RSHIFT) {
+        if (currCode == ADD || currCode == SUB || currCode == MULT || currCode == LSHIFT || currCode == RSHIFT)
+        {
             VRName = handleThree(currOp, VRName, index);
-        } else if (currCode == STORE) {
+        }
+        else if (currCode == STORE)
+        {
             VRName = handleStore(currOp, VRName, index);
-        } else if (currCode == LOAD) {
+        }
+        else if (currCode == LOAD)
+        {
             VRName = handleLoad(currOp, VRName, index);
-        } else if (currCode == LOADIL) {
+        }
+        else if (currCode == LOADIL)
+        {
             VRName = handleLoadI(currOp, VRName);
         }
 
         currOp = currOp->prev;
         index--;
 
-        if (livecount > maxlive) {
+        if (livecount > maxlive)
+        {
             maxlive = livecount;
         }
     }
-
 }
 
-int handleThree(struct Instruction* currOp, int VRName, int index) {
+int handleThree(struct Instruction *currOp, int VRName, int index)
+{
     /* Defined operand */
-    if(SRtoVR[currOp->sr3] == -1) {
+    if (SRtoVR[currOp->sr3] == -1)
+    {
         SRtoVR[currOp->sr3] = VRName;
         VRName++;
         livecount++;
@@ -125,7 +142,8 @@ int handleThree(struct Instruction* currOp, int VRName, int index) {
     // livecount--;
 
     /* used operand 1 */
-    if(SRtoVR[currOp->sr1] == -1) {
+    if (SRtoVR[currOp->sr1] == -1)
+    {
         SRtoVR[currOp->sr1] = VRName;
         VRName++;
     }
@@ -134,13 +152,15 @@ int handleThree(struct Instruction* currOp, int VRName, int index) {
     // livecount++;
 
     /* used operand 2 */
-    if(SRtoVR[currOp->sr2] == -1) {
+    if (SRtoVR[currOp->sr2] == -1)
+    {
         SRtoVR[currOp->sr2] = VRName;
         VRName++;
     }
     currOp->vr2 = SRtoVR[currOp->sr2];
     currOp->nu2 = LU[currOp->sr2];
-    if (currOp->sr2 != currOp->sr1) {
+    if (currOp->sr2 != currOp->sr1)
+    {
         livecount++;
     }
 
@@ -151,10 +171,12 @@ int handleThree(struct Instruction* currOp, int VRName, int index) {
     return VRName;
 }
 
-int handleStore(struct Instruction* currOp, int VRName, int index) {
+int handleStore(struct Instruction *currOp, int VRName, int index)
+{
     // NOTE: Treat both operands as uses here!
     /* used operand 1 */
-    if(SRtoVR[currOp->sr1] == -1) {
+    if (SRtoVR[currOp->sr1] == -1)
+    {
         SRtoVR[currOp->sr1] = VRName;
         VRName++;
     }
@@ -163,14 +185,16 @@ int handleStore(struct Instruction* currOp, int VRName, int index) {
     livecount++;
 
     /* used operand 2 */
-    if(SRtoVR[currOp->sr3] == -1) {
+    if (SRtoVR[currOp->sr3] == -1)
+    {
         SRtoVR[currOp->sr3] = VRName;
         VRName++;
     }
     currOp->vr3 = SRtoVR[currOp->sr3];
     currOp->nu3 = LU[currOp->sr3];
 
-    if (currOp->sr1 != currOp->sr3) {
+    if (currOp->sr1 != currOp->sr3)
+    {
         livecount++;
     }
 
@@ -181,9 +205,11 @@ int handleStore(struct Instruction* currOp, int VRName, int index) {
     return VRName;
 }
 
-int handleLoad(struct Instruction* currOp, int VRName, int index) {
+int handleLoad(struct Instruction *currOp, int VRName, int index)
+{
     /* Defined operand */
-    if(SRtoVR[currOp->sr3] == -1) {
+    if (SRtoVR[currOp->sr3] == -1)
+    {
         SRtoVR[currOp->sr3] = VRName;
         VRName++;
         livecount++;
@@ -195,7 +221,8 @@ int handleLoad(struct Instruction* currOp, int VRName, int index) {
     // livecount--;
 
     /* used operand 1 */
-    if(SRtoVR[currOp->sr1] == -1) {
+    if (SRtoVR[currOp->sr1] == -1)
+    {
         SRtoVR[currOp->sr1] = VRName;
         VRName++;
     }
@@ -209,9 +236,11 @@ int handleLoad(struct Instruction* currOp, int VRName, int index) {
     return VRName;
 }
 
-int handleLoadI(struct Instruction* currOp, int VRName) {
+int handleLoadI(struct Instruction *currOp, int VRName)
+{
     /* Defined operand */
-    if(SRtoVR[currOp->sr3] == -1) {
+    if (SRtoVR[currOp->sr3] == -1)
+    {
         SRtoVR[currOp->sr3] = VRName;
         VRName++;
         livecount++;
@@ -226,58 +255,61 @@ int handleLoadI(struct Instruction* currOp, int VRName) {
     return VRName;
 }
 
-void print_renamed_code(struct Instruction* ir) {
-    struct Instruction* curr = ir->next;  // Skip dummy head
-    
-    while (curr->line != -1) {  // Until we hit end sentinel
-        switch(curr->opcode) {
-            case LOAD:
-                printf("load r%d => r%d\n", 
-                    curr->vr1, curr->vr3);
-                break;
-                
-            case STORE:
-                printf("store r%d => r%d\n", 
-                    curr->vr1, curr->vr3);
-                break;
-                
-            case LOADIL:
-                printf("loadI %d => r%d\n", 
-                    curr->sr1, curr->vr3);
-                break;
-                
-            case ADD:
-                printf("add r%d, r%d => r%d\n", 
-                    curr->vr1, curr->vr2, curr->vr3);
-                break;
-                
-            case SUB:
-                printf("sub r%d, r%d => r%d\n", 
-                    curr->vr1, curr->vr2, curr->vr3);
-                break;
-                
-            case MULT:
-                printf("mult r%d, r%d => r%d\n", 
-                    curr->vr1, curr->vr2, curr->vr3);
-                break;
-                
-            case LSHIFT:
-                printf("lshift r%d, r%d => r%d\n", 
-                    curr->vr1, curr->vr2, curr->vr3);
-                break;
-                
-            case RSHIFT:
-                printf("rshift r%d, r%d => r%d\n", 
-                    curr->vr1, curr->vr2, curr->vr3);
-                break;
-                
-            case OUTPUTL:
-                printf("output %d\n", curr->sr1);
-                break;
-                
-            case NOPL:
-                printf("nop\n");
-                break;
+void print_renamed_code(struct Instruction *ir)
+{
+    struct Instruction *curr = ir->next; // Skip dummy head
+
+    while (curr->line != -1)
+    { // Until we hit end sentinel
+        switch (curr->opcode)
+        {
+        case LOAD:
+            printf("load r%d => r%d\n",
+                   curr->vr1, curr->vr3);
+            break;
+
+        case STORE:
+            printf("store r%d => r%d\n",
+                   curr->vr1, curr->vr3);
+            break;
+
+        case LOADIL:
+            printf("loadI %d => r%d\n",
+                   curr->sr1, curr->vr3);
+            break;
+
+        case ADD:
+            printf("add r%d, r%d => r%d\n",
+                   curr->vr1, curr->vr2, curr->vr3);
+            break;
+
+        case SUB:
+            printf("sub r%d, r%d => r%d\n",
+                   curr->vr1, curr->vr2, curr->vr3);
+            break;
+
+        case MULT:
+            printf("mult r%d, r%d => r%d\n",
+                   curr->vr1, curr->vr2, curr->vr3);
+            break;
+
+        case LSHIFT:
+            printf("lshift r%d, r%d => r%d\n",
+                   curr->vr1, curr->vr2, curr->vr3);
+            break;
+
+        case RSHIFT:
+            printf("rshift r%d, r%d => r%d\n",
+                   curr->vr1, curr->vr2, curr->vr3);
+            break;
+
+        case OUTPUTL:
+            printf("output %d\n", curr->sr1);
+            break;
+
+        case NOPL:
+            printf("nop\n");
+            break;
         }
         curr = curr->next;
     }
